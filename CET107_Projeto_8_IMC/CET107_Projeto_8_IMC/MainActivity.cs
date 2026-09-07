525be5ebe3b3bc8feffd89213fe2f09934592d3f
Franciele Torres <mariafrancielesm@gmail.com>
2026-09-07 22:05:48 +0100

    Implement toolbar and icon

--- CET107_Projeto_8_IMC/CET107_Projeto_8_IMC/MainActivity.cs
+++ CET107_Projeto_8_IMC/CET107_Projeto_8_IMC/MainActivity.cs
@@ -1,6 +1,9 @@
 namespace CET107_Projeto_8_IMC
 {
-    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    [Activity(Label = "@string/app_name2", 
+        Theme ="@style/AppTheme",
+        Icon ="@mipmap/appicon",
+        MainLauncher = true)]
     public class MainActivity : Activity
     {
         protected override void OnCreate(Bundle? savedInstanceState)
